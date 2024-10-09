851339f415fbf3f118b7b9133143dd569553503f
ahsan-javaid <ahsan786cs@gmail.com>
2024-10-09 13:36:49 +0500

    devop: get latest block number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+
+use alloy::providers::{Provider, ProviderBuilder};
+use eyre::Result;
 mod provider;
 
-//use crate::provider;
+#[tokio::main]
+async fn main() -> Result<()> {
+    // ...
 
+    let rpc_url = "https://eth.merkle.io".parse()?;
 
-fn main() {
-    provider::rpc::request("");
+    // Create a provider with the HTTP transport using the `reqwest` crate.
+    let provider = ProviderBuilder::new().on_http(rpc_url);
+
+    let block = provider.get_block_number().await;
+    match block {
+        Ok(val) => {
+            println!("block: {val}");
+        },
+        Err(_) => {
+            println!("error");
+        }
+    }
+
+    Ok(())
 }
